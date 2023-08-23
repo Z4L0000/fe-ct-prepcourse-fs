920a4ae378a1200369ae72405573a09389713a5b
Z4l000 <143021922+Z4L0000@users.noreply.github.com>
2023-08-23 14:38:39 -0300

    Update homework.js

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -4,7 +4,7 @@ function mayuscula(nombre) {
    // Esta función recibe un nombre (string).
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
-   // Tu código:
+   // Tu código:ASDA
 }
 
 function invocarCallback(cb) {
